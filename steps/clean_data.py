f2f335f729588b8531a0f4371ffbedf694ed5218
Niraj <nkj5@uakron.edu>
2025-03-27 09:52:30 -0400

    Remove the content of clean_data

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,6 +1,1 @@
-import yfinance as yf
-
-# function to download data
-def download_data(stock, start_date, end_date, interval):
-    data = yf.download(stock, start=start_date, end=end_date, interval=interval)
-    return data
+# clean the data
